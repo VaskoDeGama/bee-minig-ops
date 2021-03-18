cd9ca09194b45edcde27d61c824fdb9c054ec04a
vaskodegama <vasr161095@gmail.com>
2021-03-18 12:00:15 +0300

    remove server

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const router = require('./routes')
-const port = /** @type {number} */ process.env.PORT || 3000
-const host = 'localhost'
-const server = http.createServer(router)
-
-server.listen(port, host, () => {
-  console.log(`Server is running on ${host}:${port}`)
-})
